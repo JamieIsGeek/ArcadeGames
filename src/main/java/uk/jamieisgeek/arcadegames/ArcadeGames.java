2101fb87c89f906bd11ab51d4df0551d446638db
JamieOnSootMC <jamie@sootmc.net>
2023-06-11 18:03:12 +0100

    Initial commit

---
+++ src/main/java/uk/jamieisgeek/arcadegames/ArcadeGames.java
@@ -0,0 +1,17 @@
+package uk.jamieisgeek.arcadegames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArcadeGames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
